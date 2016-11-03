18a9a704d72e524fd730f30966731274294b3105
Stefan Sundin <git@stefansundin.com>
2016-11-03 15:30:04 -0700

    Rename tail-log-stream to tail-log-group.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 go build s3-versioned-bytes.go
 go build lookup-sir.go
-go build tail-log-stream.go
+go build tail-log-group.go
 go build sum-unused-ebs.go
 go build s3-versioning.go
