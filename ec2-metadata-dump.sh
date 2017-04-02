95540a5375812c13299ab002f56c037ed3ebb3c9
Stefan Sundin <git@stefansundin.com>
2017-04-02 14:19:22 -0700

    curl | bash

--- ec2-metadata-dump.sh
+++ ec2-metadata-dump.sh
@@ -1,21 +1,29 @@
 #!/bin/bash -e
-AZ=$(curl http://169.254.169.254/2016-09-02/meta-data/placement/availability-zone)
-INSTANCE_ID=$(curl http://169.254.169.254/2016-09-02/meta-data/instance-id)
-INSTANCE_TYPE=$(curl http://169.254.169.254/2016-09-02/meta-data/instance-type)
-PROFILE=$(curl http://169.254.169.254/2016-09-02/meta-data/profile)
-AMI_ID=$(curl http://169.254.169.254/2016-09-02/meta-data/ami-id)
-HOSTNAME=$(curl http://169.254.169.254/2016-09-02/meta-data/hostname)
-LOCAL_HOSTNAME=$(curl http://169.254.169.254/2016-09-02/meta-data/local-hostname)
-LOCAL_IPV4=$(curl http://169.254.169.254/2016-09-02/meta-data/local-ipv4)
-PUBLIC_HOSTNAME=$(curl http://169.254.169.254/2016-09-02/meta-data/public-hostname)
-PUBLIC_IPV4=$(curl http://169.254.169.254/2016-09-02/meta-data/public-ipv4)
-MAC=$(curl http://169.254.169.254/2016-09-02/meta-data/mac)
-INTERFACE_ID=$(curl http://169.254.169.254/2016-09-02/meta-data/network/interfaces/macs/$MAC/interface-id)
-PUBLIC_KEY=$(curl http://169.254.169.254/2016-09-02/meta-data/public-keys/0/openssh-key)
-IAM_INFO=$(curl http://169.254.169.254/2016-09-02/meta-data/iam/info)
-IAM_ROLE=$(curl http://169.254.169.254/2016-09-02/meta-data/iam/security-credentials/)
-IAM_CREDENTIALS=$(curl http://169.254.169.254/2016-09-02/meta-data/iam/security-credentials/$IAM_ROLE)
-INSTANCE_IDENTITY=$(curl http://169.254.169.254/2016-09-02/dynamic/instance-identity/document)
+# curl -fL https://raw.githubusercontent.com/stefansundin/aws/master/ec2-metadata-dump.sh | bash -e
+
+function get {
+  curl -s http://169.254.169.254/2016-09-02/$1
+}
+
+>&2 echo "Fetching metadata..."
+
+AZ=$(get meta-data/placement/availability-zone)
+INSTANCE_ID=$(get meta-data/instance-id)
+INSTANCE_TYPE=$(get meta-data/instance-type)
+PROFILE=$(get meta-data/profile)
+AMI_ID=$(get meta-data/ami-id)
+HOSTNAME=$(get meta-data/hostname)
+LOCAL_HOSTNAME=$(get meta-data/local-hostname)
+LOCAL_IPV4=$(get meta-data/local-ipv4)
+PUBLIC_HOSTNAME=$(get meta-data/public-hostname)
+PUBLIC_IPV4=$(get meta-data/public-ipv4)
+MAC=$(get meta-data/mac)
+INTERFACE_ID=$(get meta-data/network/interfaces/macs/$MAC/interface-id)
+PUBLIC_KEY=$(get meta-data/public-keys/0/openssh-key)
+IAM_INFO=$(get meta-data/iam/info)
+IAM_ROLE=$(get meta-data/iam/security-credentials/)
+IAM_CREDENTIALS=$(get meta-data/iam/security-credentials/$IAM_ROLE)
+INSTANCE_IDENTITY=$(get dynamic/instance-identity/document)
 
 echo "availability-zone: $AZ"
 echo "instance-id: $INSTANCE_ID"
