28c4534844fa6ca1a4141633d9c7f609d55677a7
Stefan Sundin <git@stefansundin.com>
2017-04-07 17:37:27 -0700

    Update federate.py to use boto3.

--- cli/federate.py
+++ cli/federate.py
@@ -15,12 +15,10 @@
 # https://aws.amazon.com/blogs/security/enable-your-federated-users-to-work-in-the-aws-management-console-for-up-to-12-hours/
 # http://boto.cloudhackers.com/en/latest/ref/sts.html
 
-import sys, os, urllib, json, requests
-from boto.sts import STSConnection
+import sys, os, urllib, json, requests, boto3
 
 dest = "https://console.aws.amazon.com/console/home"
 
-source_profile = None
 mfa_serial = None
 
 if len(sys.argv) == 2:
@@ -30,21 +28,24 @@ if len(sys.argv) == 2:
         import configparser
         config = configparser.ConfigParser()
         config.read([os.environ["HOME"]+"/.aws/credentials"])
-        if not config.has_option(sys.argv[1], "source_profile"):
-            print("Profile "+sys.argv[1]+" not found in ~/.aws/credentials.")
-            sys.exit(1)
-        source_profile = config.get(sys.argv[1], "source_profile")
-        role_arn = config.get(sys.argv[1], "role_arn")
-        mfa_serial = config.get(sys.argv[1], "mfa_serial", fallback=None)
-        region = config.get(sys.argv[1], "region", fallback=None)
-        if region:
-            dest += "?region=" + region
+        if config.has_option(sys.argv[1], "source_profile"):
+            source_profile = config.get(sys.argv[1], "source_profile")
+            boto3.setup_default_session(profile_name=source_profile)
+            role_arn = config.get(sys.argv[1], "role_arn")
+            mfa_serial = config.get(sys.argv[1], "mfa_serial", fallback=None)
+            region = config.get(sys.argv[1], "region", fallback=None)
+            if region:
+                dest += "?region=" + region
+        else:
+            response = boto3.client("sts").get_caller_identity()
+            role_arn = "arn:aws:iam::"+response["Account"]+":role/"+sys.argv[1]
 elif len(sys.argv) == 3:
     role_arn = sys.argv[1]
     mfa_serial = sys.argv[2]
 else:
     print("Insufficient arguments.")
     print("Usage: %s <profile>" % sys.argv[0])
+    print("Usage: %s <role_name>" % sys.argv[0])
     print("Usage: %s <role_arn>" % sys.argv[0])
     print("Usage: %s <role_arn> <mfa_arn>" % sys.argv[0])
     sys.exit(1)
@@ -56,29 +57,25 @@ else:
     session_name = os.environ["USER"]
 
 kwargs = {
-    "role_session_name": session_name,
-    "role_arn": role_arn,
+    "RoleArn": role_arn,
+    "RoleSessionName": session_name,
 }
 
 if mfa_serial:
-    kwargs["mfa_serial_number"] = mfa_serial
-    kwargs["mfa_token"] = raw_input("Enter MFA code: ")
+    kwargs["SerialNumber"] = mfa_serial
+    kwargs["TokenCode"] = raw_input("Enter MFA code: ")
 
-# Call AssumeRole to get temporary access keys for the federated user
-if source_profile:
-    sts_connection = STSConnection(profile_name=source_profile)
-else:
-    sts_connection = STSConnection()
-assumed_role_object = sts_connection.assume_role(**kwargs)
+sts = boto3.client("sts")
+role = sts.assume_role(**kwargs)
 
-# Make request to AWS federation endpoint to get sign-in token
+# Request a sign-in token from the AWS federation endpoint
 r = requests.get("https://signin.aws.amazon.com/federation", params={
     "Action": "getSigninToken",
     "SessionDuration": "43200",  # 12 hours
     "Session": json.dumps({
-        "sessionId": assumed_role_object.credentials.access_key,
-        "sessionKey": assumed_role_object.credentials.secret_key,
-        "sessionToken": assumed_role_object.credentials.session_token,
+        "sessionId": role["Credentials"]["AccessKeyId"],
+        "sessionKey": role["Credentials"]["SecretAccessKey"],
+        "sessionToken": role["Credentials"]["SessionToken"],
     }),
 })
 data = r.json()
@@ -89,7 +86,7 @@ data = r.json()
 url = "https://signin.aws.amazon.com/federation"
 url += "?Action=login"
 url += "&Issuer=https://github.com/stefansundin/aws/blob/master/cli/federate.py"
-url += "&Destination=" + urllib.quote_plus(dest)
+url += "&Destination=" + urllib.parse.quote_plus(dest)
 url += "&SigninToken=" + data["SigninToken"]
 
 # Print URL
