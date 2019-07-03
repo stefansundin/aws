6b5c9b0fdbfcf52f3eaae909837f2c0021bb78de
Stefan Sundin <git@stefansundin.com>
2019-07-03 15:36:03 -0700

    Add some new stuff to ec2-metadata-dump.sh.

--- ec2-metadata-dump.sh
+++ ec2-metadata-dump.sh
@@ -5,11 +5,13 @@
 # curl -fsSL https://raw.githubusercontent.com/stefansundin/aws/master/ec2-metadata-dump.sh | bash -s user-data
 
 function get {
-  curl -fsS http://169.254.169.254/2016-09-02/$1
+  curl -fsS http://169.254.169.254/2018-09-24/$1 2> /dev/null
 }
 
 >&2 echo "Fetching metadata..."
 
+DOMAIN=$(get meta-data/services/domain)
+PARTITION=$(get meta-data/services/partition)
 AZ=$(get meta-data/placement/availability-zone)
 INSTANCE_ID=$(get meta-data/instance-id)
 INSTANCE_TYPE=$(get meta-data/instance-type)
@@ -36,13 +38,28 @@ SUBNET_CIDR=$(get meta-data/network/interfaces/macs/$MAC/subnet-ipv4-cidr-block)
 SECURITY_GROUP_IDS=$(get meta-data/network/interfaces/macs/$MAC/security-group-ids)
 SECURITY_GROUPS=$(get meta-data/network/interfaces/macs/$MAC/security-groups)
 INSTANCE_IDENTITY=$(get dynamic/instance-identity/document)
+IDENTITY_INFO=$(get meta-data/identity-credentials/ec2/info)
+IDENTITY_CREDENTIALS=$(get meta-data/identity-credentials/ec2/security-credentials/ec2-instance)
+EVENTS_HISTORY=$(get meta-data/events/maintenance/history)
+EVENTS_SCHEDULED=$(get meta-data/events/maintenance/scheduled)
 
+echo "domain: $DOMAIN"
+echo "partition: $PARTITION"
 echo "availability-zone: $AZ"
 echo "instance-id: $INSTANCE_ID"
 echo "instance-type: $INSTANCE_TYPE"
 echo "profile: $PROFILE"
 echo "ami-id: $AMI_ID"
 echo "ssh key: $PUBLIC_KEY"
+
+ACTIVE_KEYS=$(get meta-data/managed-ssh-keys/active-keys/)
+echo "$ACTIVE_KEYS" | while read -r username; do
+  [ -z "$username" ] && continue
+  echo
+  echo "# managed-ssh-keys for user $username:"
+  get meta-data/managed-ssh-keys/active-keys/$username/
+done
+
 echo
 echo "hostname: $HOSTNAME"
 echo "local-hostname: $LOCAL_HOSTNAME"
@@ -67,6 +84,13 @@ if [ $? -eq 0 ]; then
   echo "iam credentials: $IAM_CREDENTIALS"
 fi
 
+echo
+# These are used by eic_harvest_hostkeys in ec2-instance-connect, not sure if they can be used for anything else
+echo "identity-credentials info: $IDENTITY_INFO"
+echo "identity-credentials credentials: $IDENTITY_CREDENTIALS"
+echo
+echo "historical events: $EVENTS_HISTORY"
+echo "scheduled events: $EVENTS_SCHEDULED"
 echo
 echo "instance-identity: $INSTANCE_IDENTITY"
 
